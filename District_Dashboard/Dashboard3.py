1c4b83489979949b0e5836fc4587966d3ac0e531
Sanjohn12 <142684542+Sanjohn12@users.noreply.github.com>
2025-07-13 19:56:06 +0530

    Update Dashboard3.py

--- District_Dashboard/Dashboard3.py
+++ District_Dashboard/Dashboard3.py
@@ -18,7 +18,7 @@ def load_data():
     # Original data (actual values)
     df = pd.read_csv("Srilanka/District_Dashboard/District_Data_Modified.csv")
     # Load shapefile
-    shp_path = "C:\Users\sanojan\Desktop\sano Tcp\srilanka shp\lka_admbnda_adm2_slsd_20220816.shp"
+    shp_path = "Srilanka/District_Dashboard/lka_admbnda_adm2_slsd_20220816.shp"
     gdf = gpd.read_file(shp_path)
     return df, gdf
 
